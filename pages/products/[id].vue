938f007cfcd9978aefe2565d4b327b24650d3685
bakemonostan <ehizomokhomion@gmail.com>
2023-11-27 10:32:04 +0100

    add meta tags

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,25 +1,27 @@
 <template>
     <div>
+
+        <Head>
+            <Title>Nuxt Dojo | {{ product.title }}</Title>
+            <Meta name="description" :content="product.description" />
+        </Head>
+
         <ProductDetails :product="product" />
     </div>
 </template>
-
-<script setup lang="ts">
-import { useRoute } from 'vue-router'
-definePageMeta({
-    layout: 'products'
-})
+  
+<script setup>
 const { id } = useRoute().params
+const uri = 'https://fakestoreapi.com/products/' + id
 
-const { data: product } = await useFetch('https://fakestoreapi.com/products/' + id,)
+//  fetch the products
+const { data: product } = await useFetch(uri, { key: id })
+
 if (!product.value) {
-    throw createError({
-        statusCode: 404,
-        statusMessage: 'Product not found',
-        fatal: false
-    })
+    throw createError({ statusCode: 404, statusMessage: 'Product not found' })
 }
 
+definePageMeta({
+    layout: "products",
+})
 </script>
-
-<style scoped></style>
